550f99bb2cf68250bacd1f398bc69f810be0c5e0
Jose Horta <joshhortt@yahoo.com>
2019-04-16 17:14:27 +0100

    add var's with if' else' statements; add also submit function with hide', show', text', methods.

--- script.js
+++ script.js
@@ -13,4 +13,32 @@ $(document).ready(function() {
     $('#uppercase').show();
     event.preventDefault();
   });
+  
+
+  var leapYear = function(year) {
+  if (year % 400 === 0) {
+    return true;
+  }
+  if ((year % 4 === 0) && (year % 100 !== 0)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+  $("form#leap_year").submit(function(event) {
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+
+    $(".year").text(year);
+    if (result) {
+      $(".not").hide();
+    } else {
+      $(".not").show();
+    }
+
+    $("#result").show();
+    event.preventDefault();
+  });
+
 });
