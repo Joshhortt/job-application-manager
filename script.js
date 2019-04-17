8ea2d60dbbd5c9882b1f07f5c1cdd98492adf255
Jose Horta <joshhortt@yahoo.com>
2019-04-17 11:06:53 +0100

    add new functions with .parseInt', .each', .change', .val' methods, in script.js.

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+
+  // First application
   $('#blanks form').submit(function(event) {
 
     var letter_personInput = $("input#letter_person").val();
@@ -7,14 +9,15 @@ $(document).ready(function() {
     event.preventDefault();
   });
 
+// Second application
     $('#blanks form').submit(function(event) {
     var confirmAskInput = $("input#confirmAsk").val();
     $('.confirmAsk').text(confirmAskInput);
     $('#uppercase').show();
     event.preventDefault();
   });
-  
 
+// Third application
   var leapYear = function(year) {
   if (year % 400 === 0) {
     return true;
@@ -40,5 +43,20 @@ $(document).ready(function() {
     $("#result").show();
     event.preventDefault();
   });
+
+// Fourth application
+  function calcscore() {
+    var score = 0;
+    $(".calc:checked").each(function() {
+    score += parseInt($(this).val(), 10);
+    });
+    $("input[name=sum]").val(score)
+  }
+
+    $().ready(function() {
+    $(".calc").change(function() {
+    calcscore()
+    });
+ });
 
 });
